d47105753b84e49b30e2a8348710dd61843b40cb
opeyemi-code <opeyemitoheeb31@gmail.com>
2023-09-11 21:09:26 +0100

    fix the sidebar

--- src/sidebar.jsx
+++ src/sidebar.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function Sidebar(props) {
   const noteElement = props.notes.map((note,index)=> (
     <div key={note.id}>
-        <div className={`title ${note.id === props.currentNote.id ? "selected-note" : ""}`} onClick={()=> props.setCurrentNoteId(note.id)}>
+        <div className={`title ${note === props.setCurrentNoteId ? "selected-note" : ""}`} onClick={()=> props.setCurrentNoteId(note)}>
          <h4>Note {index + 1}</h4>
         </div>
     </div>
